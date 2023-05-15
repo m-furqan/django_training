7b9df493eadc0eb9d45e252121026e6d7bc634a3
m-furqan <furqan.nasir@arbisoft.com>
2023-05-15 20:57:59 +0500

    fix models and migrrationn command issues

--- products/management/commands/populate_data.py
+++ products/management/commands/populate_data.py
@@ -1,8 +1,8 @@
 import json
 import random
 
-from django.core.management.base import BaseCommand, CommandError
-from products.models import Category, Inventory, Product
+from django.core.management.base import BaseCommand
+from products.models import Category, Product, Sku
 
 class Command(BaseCommand):
     help = 'Populate data from JSON file'
@@ -10,20 +10,57 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str, help='The JSON file to read data from')
 
+    # def create_or_return_category(self, category_name):
+    #     try:
+    #         category = Category.objects.get(name=category_name)
+    #         return category
+
+    #     except Category.DoesNotExist:
+    #         return Category.objects.create(name=category_name)
+
     def handle(self, *args, **options):
         filename = options['filename']
         with open(filename, 'r') as file:
             data = json.load(file)
         
         for product in data:
-            category = Category.objects.create(name=product['category'][0])
-            inventory = Inventory.objects.create(quantity=random.randint(1, 100))
-            
-            Product.objects.create(
-                name=product['name'],
-                description=product['description'],
-                category_id=category,
-                inventory_id=inventory,
-                price=product['price']
+            parent_category_name = None
+
+            for category_name in product['category']:
+                category_name = category_name.lower()
+                category, _ = Category.objects.get_or_create(name=category_name)
+
+                if parent_category_name and not category.parent:
+                    category.parent, _ = Category.objects.get_or_create(name=parent_category_name)
+                    category.save()
+                
+                parent_category_name = category_name
+
+            category, _ = Category.objects.get_or_create(name=product['category'][-1])
+            item = Product.objects.create(
+                retail_id = product['retailer_sku'],
+                gender = product['gender'],
+                brand = product['brand'],
+                market = product['market'],
+                name = product['name'],
+                description = product['description'],
+                care = ", ".join(product['care']),
+                image_urls = product['image_urls'],
+                category = category,
             )
+
+            skus_to_bulk_create = []
+            for sku in product['skus'].values():
+                sku_data = {
+                    'is_out_of_stock': sku.get('out_of_stock') or False,
+                    'price': sku['price'],
+                    'size': sku['size'],
+                    'colour': sku.get('color') or 'UniColour',
+                    'currency': sku['currency'],
+                }
+                sku = Sku(product=item, **sku_data)
+                skus_to_bulk_create.append(sku)
+
+            Sku.objects.bulk_create(skus_to_bulk_create)
+
         self.stdout.write(self.style.SUCCESS('Data successfully populated'))
--- products/models.py
+++ products/models.py
@@ -2,22 +2,41 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
-
-class Inventory(models.Model):
-    quantity = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
+    retail_id = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+    brand = models.CharField(max_length=100)
+    market = models.CharField(max_length=100)
     name = models.CharField(max_length=200)
     description = models.TextField()
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    inventory_id = models.ForeignKey(Inventory, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    care = models.TextField()
+    image_urls = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Sku(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='skus')
+    is_out_of_stock = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=20)
+    colour = models.CharField(max_length=50)
+    currency = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.colour} - {self.size}"
